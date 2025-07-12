28e694c59329e0d5f33bb9fc454e3e5a11fc789d
ZeAlexandre5 <josealexandredapaixaodias551@gmail.com>
2025-07-12 10:33:27 -0300

    Adição da Página reserva e um proto-banco de dados

--- SARC/models.py
+++ SARC/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Reserva(models.Model):
+    id_reserva = models.AutoField(primary_key=True)
+    data = models.DateField()
+    horario = models.TimeField()
+    sala = models.CharField(max_length=100)
+    motivo = models.TextField()
+    presenca = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Reserva {self.id_reserva} - {self.data} {self.horario} - {self.sala} - {self.motivo} - {'Presente' if self.presenca else 'Ausente'}"
--- SARC/views.py
+++ SARC/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    return render(request,"Sarc/index.html")
 
 def reserva(request):
-    return render(request,"reservas.html")
+    return render(request,"Sarc/reservas.html")
 
 def salas(request):
-    return render(request,"salas.html")
+    return render(request,"Sarc/salas.html")
 
 def cadastro(request):
-    return render(request,"cadastro.html")
+    return render(request,"Sarc/cadastro.html")
