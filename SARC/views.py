a44e01bb448c8101631e6d12e80361cf20a236c8
ZeAlexandre5 <josealexandredapaixaodias551@gmail.com>
2025-07-26 09:27:41 -0300

    Revert "teste"
    This reverts commit c875bb8e97361bdc6825e0b0561dd6622dd3e842.

--- SARC/views.py
+++ SARC/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    return render(request,"Sarc/index.html")
 
 def reserva(request):
     return render(request,"Sarc/reservas.html")
